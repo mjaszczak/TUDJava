cbd6dfb682d52a632576dfb05ada3d32f428fa0f
mjaszczak <mjaszczak@sigma.ug.edu.pl>
2016-01-13 01:20:49 +0100

    laptop changess

--- Projekt2/src/main/java/com/example/projekt/domain/Laptop.java
+++ Projekt2/src/main/java/com/example/projekt/domain/Laptop.java
@@ -17,5 +17,36 @@ public class Laptop implements java.io.Serializable{
     private String model;
     private Procesor procesor;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    public String getFirma() {
+        return firma;
+    }
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_procesor", nullable = false)
+    public Procesor getProcesor() {
+        return procesor;
+    }
+    public void setProcesor(Procesor procesor) { this.procesor = procesor; }
+
 
 }
