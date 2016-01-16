c6b367e299333a24421ca1132461557be8ff77ef
mjaszczak <mjaszczak@sigma.ug.edu.pl>
2016-01-16 19:44:45 +0100

    Procesor class

--- Projekt2/src/main/java/com/example/projekt/domain/Laptop.java
+++ Projekt2/src/main/java/com/example/projekt/domain/Laptop.java
@@ -1,6 +1,5 @@
 package com.example.projekt.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.*;
---
+++ Projekt2/src/main/java/com/example/projekt/domain/Procesor.java
@@ -0,0 +1,51 @@
+package com.example.projekt.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Procesor.wszystkie", query = "SELECT p FROM Procesor p"),
+})
+
+public class Procesor {
+    private Long id;
+    private String procesor;
+    private String opis;
+
+    private List<Laptop> laptopy = new ArrayList<Laptop>();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @OneToMany(mappedBy = "laptop", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    public List<Laptop> getLaptopy() {
+        return laptopy;
+    }
+    public void setLaptopy(List<Laptop> laptopy) {
+        this.laptopy =  laptopy;
+    }
+
+    @Column(nullable = false)
+    public String getProcesor() {
+        return procesor;
+    }
+    public void setProcesor(String procesor) {
+        this.procesor = procesor;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+}
